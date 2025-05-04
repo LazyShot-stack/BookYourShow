7c48f5d00a51c359c908511cc307e153d70b857b
LazyShot-stack <dhruvikpatel48@gmail.com>
2025-05-04 14:52:25 +0530

    Initial ticket booking system

---
+++ src/test/java/com/dhruvik/ticketbookingsystem/TicketBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhruvik.ticketbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
